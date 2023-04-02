033ec8d0521793c5732ef1b20d0e64b67a7c5638
simonanderson16 <uhx8bu@virginia.edu>
2023-04-02 19:33:57 -0400

    made start button fetch questions based on user input

--- trivia/src/Trivia.js
+++ trivia/src/Trivia.js
@@ -27,27 +27,51 @@ export default function Trivia() {
 
     //===================================================
 
+    const generateQuestions = (setData) => {
+        let url = "https://opentdb.com/api.php?";
+        // if amount is 0, stop program
+        if(numQuestions === "0") {
+            return;
+        } else {
+            url = url + "amount=" + numQuestions;
+        }
+        // else, add "amount=__" where __ is the value from the QuestionsDropdown
+        //url = url + "amount=1";
+    
+        // if category is Any category, continue
+        // else, add "&category=__" where __ is the value from the CategoryDropdown
+        if(category !== "") {
+            url = url + "&category=" + category;
+        }
+
+        console.log(url);
+    
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => setData(data.results));
+    };
+
     return(
         <>
             <h2>Questions:</h2>
             <QuestionsDropdown handleGetNumQuestions={GetNumQuestions}/>
             <h2>Category:</h2>
             <CategoryDropdown handleGetCategory={GetCategory}/>
-            <Button variant="contained">Start</Button>
+            <Button variant="contained" onClick={() => generateQuestions(setQuestions)}>Start</Button>
         </>
     );
 }
 
-const generateQuestions = (setData) => {
-    let url = "https://opentdb.com/api.php?";
-    // if amount is 0, stop program
-    // else, add "amount=__" where __ is the value from the QuestionsDropdown
-    url = url + "amount=1";
+// const generateQuestions = (setData) => {
+//     let url = "https://opentdb.com/api.php?";
+//     // if amount is 0, stop program
+//     // else, add "amount=__" where __ is the value from the QuestionsDropdown
+//     url = url + "amount=1";
 
-    // if category is Any category, continue
-    // else, add "&category=__" where __ is the value from the CategoryDropdown
+//     // if category is Any category, continue
+//     // else, add "&category=__" where __ is the value from the CategoryDropdown
 
-    fetch(url)
-        .then((res) => res.json())
-        .then((data) => setData(data.results));
-};
+//     fetch(url)
+//         .then((res) => res.json())
+//         .then((data) => setData(data.results));
+// };
